d7f9f8474c1d8e553edde48f4bca423499eac889
mirel <mirel.t.lazar@gmail.com>
2019-08-15 16:19:53 +0300

    with captcha test

--- tsr.py
+++ tsr.py
@@ -21,6 +21,57 @@ args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
 
+def captch_ex(img):
+    img = img
+
+    img_final = img.copy()
+    img2gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(img2gray, 180, 255, cv2.THRESH_BINARY)
+    image_final = cv2.bitwise_and(img2gray, img2gray, mask=mask)
+    ret, new_img = cv2.threshold(image_final, 180, 255, cv2.THRESH_BINARY)  # for black text , cv.THRESH_BINARY_INV
+    '''
+            line  8 to 12  : Remove noisy portion 
+    '''
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,
+                                                         3))  # to manipulate the orientation of dilution , large x means horizonatally dilating  more, large y means vertically dilating more
+    dilated = cv2.dilate(new_img, kernel, iterations=9)  # dilate , more the iteration more the dilation
+
+    # for cv2.x.x
+
+    _, contours, hierarchy = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # findContours returns 3 variables for getting contours
+
+    # for cv3.x.x comment above line and uncomment line below
+
+    #image, contours, hierarchy = cv2.findContours(dilated,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+
+
+    for contour in contours:
+        # get rectangle bounding contour
+        [x, y, w, h] = cv2.boundingRect(contour)
+
+        # Don't plot small false positives that aren't text
+        if w < 35 and h < 35:
+            continue
+
+        # draw rectangle around contour on original image
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
+
+        '''
+        #you can crop image and send to OCR  , false detected will return no text :)
+        cropped = img_final[y :y +  h , x : x + w]
+
+        s = file_name + '/crop_' + str(index) + '.jpg' 
+        cv2.imwrite(s , cropped)
+        index = index + 1
+
+        '''
+    # write original image with added contours to disk
+    #cv2.imshow('captcha_result', img)
+    #cv2.waitKey()
+    cv2.imwrite ("captcha.png", img)
+
+#captch_ex (image)
+
 #this is RED!
 lower_col1 = np.array ([0,  50,  50])
 upper_col1 = np.array ([10, 255, 255])
@@ -31,7 +82,7 @@ upper_col2 = np.array ([180, 255, 255])
 # resize the frame, blur it, and convert it to the HSV
 # color space
 frame = imutils.resize (image, width=600)
-image = frame.copy ()
+ori_image = frame.copy ()
 #frame = imutils.rotate(frame, angle=180)
 blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
@@ -50,8 +101,8 @@ mask = cv2.dilate (mask, None, iterations=2)
 
 cv2.imwrite ("mask.png", mask)
 #detect circles
-circles = cv2.HoughCircles (mask, cv2.HOUGH_GRADIENT, 1, 40,
-    param1=100, param2=20, minRadius=20, maxRadius=200)
+circles = cv2.HoughCircles (mask, cv2.HOUGH_GRADIENT, 1, 60,
+    param1=100, param2=20, minRadius=30, maxRadius=200)
 #process circles
 c_x = 0
 c_y = 0
@@ -78,25 +129,22 @@ if circles is not None:
 #cv2.circle (mask,(c_x, c_y),c_r,(0,255,0),2)
 # draw the center of the circle
 #cv2.circle (mask,(c_x, c_y),2,(0,0,255),3)
-#save cropped image
-    uw = int(c_r/2 + c_r/6)
-    uh = int(c_r/2)
+    #crop image
+    uw = int(c_r * 65 / 100)
+    uh = int(c_r * 50 / 100)
     print ("max circle at {},{}r{} / image size: {}x{}".format(c_x, c_y, c_r, uw*2, uh*2))
+    image = ori_image.copy()
     image = image[c_y - uh:c_y + uh, c_x - uw:c_x + uw]
     #mask  = mask[c_y - uh:c_y + uh, c_x - uw:c_x + uw]
-    try:
-      if image[0].size > 0:
-        cv2.imwrite ("image.png", image)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        #gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        #mask_inv = cv2.bitwise_not(mask)
-        #gray = cv2.bitwise_and (gray, gray, mask = mask_inv)
-        cv2.imwrite ("gray.png", gray)
-        #cv2.imwrite ("mask.png", mask)
-        #use tesserocr
-        print (tesserocr.image_to_text (Image.fromarray(gray)))  # print ocr text from image
-    except:
-      print ("OCR failed due to zero image size")
+    cv2.imwrite ("image.png", image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    #mask_inv = cv2.bitwise_not(mask)
+    #gray = cv2.bitwise_and (gray, gray, mask = mask_inv)
+    cv2.imwrite ("gray.png", gray)
+    #cv2.imwrite ("mask.png", mask)
+    #use tesserocr
+    print (tesserocr.image_to_text (Image.fromarray(gray)))  # print ocr text from image
 # show the output images
 # cv2.imshow("Image", image)
 #cv2.imshow("Output", gray)
