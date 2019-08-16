d925a6409d5133a583dc9c13cd409f3778172c1c
mirel <mirel.t.lazar@gmail.com>
2019-08-16 13:33:16 +0300

    so so working

--- tsr.py
+++ tsr.py
@@ -11,6 +11,9 @@ import os
 import numpy as np
 import imutils
 
+#import random as rng
+#rng.seed(12345)
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
@@ -33,7 +36,7 @@ def captch_ex(img):
     '''
     # to manipulate the orientation of dilution ,  large x means horizonatally dilating  more,
     #    large y means vertically dilating more
-    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (15, 3))
     dilated = cv2.dilate(new_img, kernel, iterations=9)  # dilate , more the iteration more the dilation
 
     # for cv2.x.x
@@ -70,8 +73,6 @@ def captch_ex(img):
     #cv2.waitKey()
     cv2.imwrite ("captcha.png", img)
 
-#captch_ex (image)
-
 #this is RED!
 lower_col1 = np.array ([0,  50,  50])
 upper_col1 = np.array ([10, 255, 255])
@@ -99,7 +100,7 @@ mask = mask0 + mask1
 mask = cv2.erode (mask, None, iterations=2)
 mask = cv2.dilate (mask, None, iterations=2)
 
-cv2.imwrite ("mask.png", mask)
+#cv2.imwrite ("mask.png", mask)
 #detect circles
 circles = cv2.HoughCircles (mask, cv2.HOUGH_GRADIENT, 1, 60,
     param1=100, param2=20, minRadius=30, maxRadius=200)
@@ -107,11 +108,12 @@ circles = cv2.HoughCircles (mask, cv2.HOUGH_GRADIENT, 1, 60,
 c_x = 0
 c_y = 0
 c_r = 0
+cidx = 1
 if circles is not None:
   #circles = np.uint16 (np.around (circles))
   for i in circles[0,:]:
     # draw the outer circle
-    cv2.circle (frame,(i[0], i[1]), i[2],(0,255,0),2)
+    cv2.circle (frame, (i[0], i[1]), i[2], (0,255,0), 2)
     #
     cv2.imwrite ("frame.png", frame)
     
@@ -130,19 +132,40 @@ if circles is not None:
 # draw the center of the circle
 #cv2.circle (mask,(c_x, c_y),2,(0,0,255),3)
     #crop image
-    uw = int(c_r * 65 / 100)
-    uh = int(c_r * 50 / 100)
+    uw = int(c_r * 85 / 100)
+    uh = int(c_r * 65 / 100)
     print ("max circle at {},{}r{} / image size: {}x{}".format(c_x, c_y, c_r, uw*2, uh*2))
     image = ori_image.copy()
     image = image[c_y - uh:c_y + uh, c_x - uw:c_x + uw]
     #mask  = mask[c_y - uh:c_y + uh, c_x - uw:c_x + uw]
-    cv2.imwrite ("image.png", image)
+    iname = "image-{}.png".format(cidx)
+    cv2.imwrite (iname, image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #
+    """#
+    #--- performing Otsu threshold ---
+    ret,thresh1 = cv2.threshold (gray, 0, 255,cv2.THRESH_OTSU|cv2.THRESH_BINARY_INV)
+    #--- choosing the right kernel
+    #--- kernel size of 3 rows (to join dots above letters 'i' and 'j')
+    #--- and 10 columns to join neighboring letters in words and neighboring words
+    rect_kernel = cv2.getStructuringElement (cv2.MORPH_RECT, (5, 1))
+    dilation = cv2.dilate (thresh1, rect_kernel, iterations = 1)
+    #---Finding contours ---
+    _, contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    im2 = gray.copy()
+    for cnt in contours:
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    iname = "contours-{}.png".format(cidx)
+    cv2.imwrite (iname, im2)
+    """#
     #gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     #mask_inv = cv2.bitwise_not(mask)
     #gray = cv2.bitwise_and (gray, gray, mask = mask_inv)
-    cv2.imwrite ("gray.png", gray)
+    iname = "gray-{}.png".format(cidx)
+    cv2.imwrite (iname, gray)
     #cv2.imwrite ("mask.png", mask)
+    cidx = cidx + 1
     #use tesserocr
     print (tesserocr.image_to_text (Image.fromarray(gray)))  # print ocr text from image
 # show the output images
