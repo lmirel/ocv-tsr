f46d7834b341351565dfabf9a9787ace6e3641bf
mirel <mirel.t.lazar@gmail.com>
2019-09-24 23:29:41 +0200

    various tests with ramdisk

--- jetson-nano/camera_acq_video.py
+++ jetson-nano/camera_acq_video.py
@@ -1,9 +1,25 @@
 import cv2
 import numpy as np
- 
-# Create a VideoCapture object
+
 from jetcam.csi_camera import CSICamera                                                                                                                                                                                                                                         
 
+#OS side
+# sudo mount -t tmpfs -o size=2048M tmpfs /media/ramdisk
+#/etc/fstab none /media/ramdisk tmpfs nodev,nosuid,noexec,nodiratime,size=2048M 0 0
+# https://www.techrepublic.com/article/how-to-use-a-ramdisk-on-linux/
+
+#FPS computation
+from datetime import datetime
+lFps_sec = 0 #current second
+lFPSbeg = 0  #the seccond we started to run
+lFPSrun = 0  #running for X secconds
+lFPSfnm = 0  #number of frames
+#
+lFps_c = 0 #current fps
+lFps_k = 0 #current frames
+lFps_M = 0 #max fps
+use_display = True
+# Create a VideoCapture object
 #capture = cv2.VideoCapture(0)
 capture = CSICamera (width=1280, height=720)                                                                                                                                                                                                                                     
 
@@ -13,16 +29,34 @@ capture = CSICamera (width=1280, height=720)
  
 # Default resolutions of the frame are obtained.The default resolutions are system dependent.
 # We convert the resolutions from float to integer.
-w=1280 #int(capture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH ))
-h=720  #int(capture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT ))
+w   = 1280 #int(capture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH ))
+h   = 720  #int(capture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT ))
+fps = 60
 # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
 fourcc = cv2.VideoWriter_fourcc(*'XVID')  # cv2.VideoWriter_fourcc() does not exist
-video_writer = cv2.VideoWriter("output.avi", fourcc, 25, (w, h))
+#video_writer = cv2.VideoWriter("output.avi", fourcc, 30, (w, h))
+video_writer = cv2.VideoWriter("/media/ramdisk/output.avi", fourcc, 30, (w, h))
 # record video
+lFPSbeg = datetime.now()
 while True:
     frame = capture.read()
     video_writer.write(frame)
-    cv2.imshow('Video Stream', frame)
+    #
+    #fps computation
+    lFPSrun = (datetime.now() - lFPSbeg).seconds
+    lFPSfnm = lFPSfnm + 1
+    if lFPSrun > 0:
+      FPSavg = lFPSfnm / lFPSrun
+    else:
+      FPSavg = 0
+    cfpst = "FPS: %d t: %dsec %.2ffps"%(lFPSfnm, lFPSrun, FPSavg)
+    if use_display:
+      if True or (lFPSfnm % 10) == 0:
+        cv2.putText (frame, cfpst, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.imshow('Video Stream', frame)
+    else:
+      if (lFPSfnm % 50) == 0:
+        print (cfpst)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 capture.release()
